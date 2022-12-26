e2ae5fded0ffbad159d762bb159d8674c84c4b50
Jorge Tinoco <jorgel.tinoco@gmail.com>
2022-12-26 16:01:59 -0600

    Pushing from Sublime merge

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            // Testing push from VS 2022
+            // Testing push from VS 2022 -and now from sublime merge
             Secondary AuxiliarClass = new Secondary();
             Entity  EntityModel =  AuxiliarClass.CreateInstance();
             Console.WriteLine(string.Format( "This is the description for the entity model : {0}", EntityModel.Description) );
