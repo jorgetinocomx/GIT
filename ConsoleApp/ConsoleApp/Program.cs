0350a8c97dc4218d63946193c79334ba3b4f0a3e
Jorge Tinoco <jorgel.tinoco@gmail.com>
2022-12-26 15:55:42 -0600

    Testing push from VS 2022

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -6,6 +6,7 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            // Testing push from VS 2022
             Secondary AuxiliarClass = new Secondary();
             Entity  EntityModel =  AuxiliarClass.CreateInstance();
             Console.WriteLine(string.Format( "This is the description for the entity model : {0}", EntityModel.Description) );
